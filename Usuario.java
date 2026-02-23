77fa5d586cef72d3fef0a1cdcc035e1a99e83017
Moon-collab1 <churches1229@gmail.com>
2026-02-23 13:53:54 +0000

    sasa

--- Usuario.java
+++ Usuario.java
@@ -9,7 +9,35 @@ public class Usuario {
         this.email = email;
     }
 
-  
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return id + "," + nombre + "," + email;
+    }
 }
+    
 
 
